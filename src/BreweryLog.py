9352377c9b40ec66fda7af86a854379fda198a18
Austin Shinkle <austinshinkle@gmail.com>
2024-09-08 09:29:08 +0200

    integrated outside sensors

--- src/BreweryLog.py
+++ src/BreweryLog.py
@@ -16,6 +16,10 @@ DEBUG = 1
 server_ip = "ashinkl-rpiz2w"
 server_port = 12345
 
+# socket connection information (outside)
+outside_server_ip = "ashinkl-rpipw"
+outside_server_port = 12345
+
 # TEST
 socket_err_cnt = 0
 
@@ -27,10 +31,12 @@ BUCKET = 'OstentatiousBrewing'
 INFLUXDB_API_TOKEN = 'l8yvgryXx262i3ilLlDo4CTaxOJPPfFaAtvuU7w8_Pm6BCyAV-LeDqLC4BO9qDPZPzlTtHPUcxFHWF21tKVx2Q=='
 INFLUXDB_ORG = 'home'
 DB_WRITE_FREQ = 10
+GET_SENSOR_DATA_FREQ = 4
 
 # global variables to communicate between threads
 terminate_thread = 0
 new_data_avail = False
+new_outside_data_avail = False
 
 # function that gets the values of the data
 # and stores the values in global variables
@@ -62,6 +68,8 @@ def get_sensor_data():
 			
 			# get sensor data from server
 			sensor_data = json.loads(data.decode())
+
+			print(sensor_data)
 			
 			# parse dictionary data
 			# dictionary elements: 
@@ -85,12 +93,71 @@ def get_sensor_data():
 		except socket.gaierror:
 			socket_err_cnt += 1
 			print(f"Socket Error Number: {socket_err_cnt}")
+			terminate_thread = True ##DEFUGGING ONLY
 
 		except ConnectionRefusedError:
 			print("Cannot connect to server...will try again later.")
 		
 		finally:
-			sleep(1)
+			sleep(GET_SENSOR_DATA_FREQ)
+
+# function that gets the values of the data
+# and stores the values in global variables
+# designed to be run in a thread 
+def get_outside_sensor_data():
+	
+	global terminate_thread
+	global outside_temp
+	global outside_pressure
+	global outside_humidity
+	global new_outside_data_avail
+	
+	while not terminate_thread:
+		
+		print("Running Thread: get_outside_sensor_data")
+		
+		try:
+			# connect to the server which has the data
+			client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			client_socket.connect((outside_server_ip,outside_server_port))
+
+			# get the data from the socket
+			data = client_socket.recv(1024)
+			
+			if DEBUG > 0:
+				print("Received from server:", data.decode())
+			
+			# get sensor data from server
+			outside_sensor_data = json.loads(data.decode())
+			
+			# parse dictionary data
+			# dictionary elements: 
+				# Temperature_C
+				# RelativePressure_hPa
+				# Humidity_%
+				
+			print(outside_sensor_data)
+
+			outside_temp = outside_sensor_data["Temperature_C"]
+			outside_temp = round((9 * float(outside_temp))/5  + 32,1)
+			outside_pressure = outside_sensor_data["RelativePressure_hPa"]
+			outside_humidity = outside_sensor_data["Humidity_%"]
+
+			# inform the other thread new data is available
+			new_outside_data_avail = True
+
+			client_socket.close()
+
+		except socket.gaierror:
+			socket_err_cnt += 1
+			print(f"Socket Error Number: {socket_err_cnt}")
+			terminate_thread = True ##DEFUGGING ONLY
+
+		except ConnectionRefusedError:
+			print("Cannot connect to server...will try again later.")
+		
+		finally:
+			sleep(GET_SENSOR_DATA_FREQ)
 
 # function that get the received
 # values from the global variables
@@ -104,8 +171,11 @@ def write_data_to_db():
 	global fermentation_chamber_temp_1
 	global fermentation_chamber_temp_2
 	global kegerator_temp
+	global outside_temp
+	global outside_pressure
+	global outside_humidity
 	global new_data_avail
-	global count
+	global new_outside_data_avail
 	
 	while not terminate_thread:
 		
@@ -129,6 +199,26 @@ def write_data_to_db():
 				# set the data to stale
 				new_data_avail = False
 
+						# only write data if it is not stale
+			if new_outside_data_avail:
+
+				print("Writing outside values to database...")
+
+				# create a point and write it to the database - Outside Temperature
+				p = Point("Outside").tag("location", "Patio").field("temperature", outside_temp)
+				write_api.write(bucket=BUCKET, record=p)
+
+				# create a point and write it to the database - Outside Pressure
+				p = Point("Outside").tag("location", "Patio").field("pressure", outside_pressure)
+				write_api.write(bucket=BUCKET, record=p)
+
+				# create a point and write it to the database - Outside Humidity
+				p = Point("Outside").tag("location", "Patio").field("humidity", outside_humidity)
+				write_api.write(bucket=BUCKET, record=p)	
+
+				# set the data to stale
+				new_outside_data_avail = False
+
 		except ConnectionRefusedError:
 			print("Cannot connect to server...will try again later.")
 		
@@ -150,17 +240,26 @@ try:
 	# create the thread to get the information from the server
 	thread_get_sensor_data = threading.Thread(target=get_sensor_data)
 
+	# create the thread to get the information from the outsude server
+	thread_get_outside_sensor_data = threading.Thread(target=get_outside_sensor_data)
+
 	# create the thread to write the information to the database
 	thread_write_data_to_db = threading.Thread(target=write_data_to_db)
 
-	# keep the threads alive while the 
+	# keep the threads alive and restarts them if they crash
 	while True:
-	
+
+
 		if not thread_get_sensor_data.is_alive():
 			thread_get_sensor_data.start()	
 
+		if not thread_get_outside_sensor_data.is_alive():
+			thread_get_outside_sensor_data.start()	
+
 		if not thread_write_data_to_db.is_alive():
 			thread_write_data_to_db.start()
+
+		sleep(1)
 
 except KeyboardInterrupt:
 	terminate_thread = 1
